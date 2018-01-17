a92f32a05ff37654e4e8934e7400889a4ba9cb93
Ilyuza <Gizyaschwann@gmail.com>
2018-01-17 23:34:03 +0300

    Update Multithreading tasks

--- src/HW1/Main.java
+++ src/HW1/Main.java
@@ -1,23 +1,56 @@
 package HW1;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.regex.*;
+
 /**
  * Created by Ilyuza on 1/16/2018.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        File files = new File("src/HW1/resources");
+        HashMap<String, Integer> dictionary = new HashMap<>();
 
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from one more thread:P");
-                // Начало чтения и вывод - получается несколько потоков должны иметь общий доступ к одному выводу
-                // Обработка файла и изменение консольной информации
-                // Просто динамически меняющийся поток слов с кол-вом его вхождений
+        for(File item : files.listFiles()){
+                System.out.println(item.getName());
+        }
 
+            for(File item : files.listFiles()){
+                Thread thread = new Thread(new Runnable() {
+                    String line = "";
+
+                    @Override
+                    public void run() {
+                        try {
+
+                            FileReader fileReader =
+                                    new FileReader(item);
+                            BufferedReader bufferedReader =
+                                    new BufferedReader(fileReader);
+
+                            while((line = bufferedReader.readLine()) != null) {
+                                System.out.println(line);
+                                System.out.println("GGGGGG");
+                            }
+
+                            bufferedReader.close();
+
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                thread.start();
             }
-        });
-        thread1.start();
 
         System.out.println("Halo from main");
+
+        Pattern regexp = Pattern.compile("a*b*");
+        Matcher m = regexp.matcher("aaaaabc");
+        boolean b = m.matches();
+        System.out.println(b);
     }
 }
